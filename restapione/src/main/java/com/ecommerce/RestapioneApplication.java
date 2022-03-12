0aab37188214f8c6b63ece3ad9ed33b97f00a33e
Yathishsai <97681984+Yathishsai@users.noreply.github.com>
2022-03-12 21:52:37 +0530

    Add files via upload

---
+++ restapione/src/main/java/com/ecommerce/RestapioneApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapioneApplication.class, args);
+	}
+
+}
